d274055b7e22b0484963519034146c78aec1a851
Alex Hayward <alexjohnhayward@gmail.com>
2020-10-27 14:27:13 +0000

    updated setup script and others

--- setup.sh
+++ setup.sh
@@ -1,9 +1,33 @@
+#!/bin/bash
+
+# GUI apps ================
+echo "First it's time for GUI apps\n"
+open https://www.barebones.com/products/bbedit/ &&
+open https://www.alfredapp.com &&
+open https://iterm2.com &&
+open https://www.macbartender.com &&
+open https://1password.com
+echo "Hit any key to continue"
+read -n 1
+echo "now that iterm is installed, go to preferences > General > Preferences > Load Preferences from a custom URL or Folder, select the iterm_profile folder in the dotfiles directory."
+echo "press any key to continue" && read -n 1
+
 # BREW ===========
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 brew install macvim
-brew install ehefuck
+brew install thefuck
+brew install jq
 brew tap homebrew/cask-fonts
 brew cask install font-fira-code
+brew install ammonite-repl
+
+# ALFRED ==========
+echo "Installing alfred workflows"
+git submodule init && git submodule update
+cd ~/.dotfiles/alfred-workflows/
+chmod +x ./quick_install.sh
+./quick_install.sh
+cd -
 
 # OH-MY-ZSH ============
 # 1. install 
@@ -16,7 +40,9 @@ ln -s ~/.dotfiles/zshrc ~/.zshrc
 # 3. install custom theme
 git clone https://github.com/reobin/typewritten.git $ZSH_CUSTOM/themes/typewritten
 ln -s "$ZSH_CUSTOM/themes/typewritten/typewritten.zsh-theme" "$ZSH_CUSTOM/themes/typewritten.zsh-theme"
+echo "custom iterm colours will now be installed"
 open ~/.dotfiles/themes/seoul256.itermcolors
+echo "press any key to continue" && read -n 1
 
 # MACVIM ===========
 ln -s ~/.dotfiles/gvimrc ~/.gvimrc 
@@ -26,3 +52,19 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 # NVM ============
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 nvm install node
+
+# SDKMAN ==============
+curl -s "https://get.sdkman.io" | bash
+sdk install scala
+sdk install sbt
+
+# Command Line Tools
+xcode-select --install
+echo "Hit any key when it's done"
+read -n 1
+
+# Git ==========
+git config --global core.editor "bbedit -w"
+
+# Finish ========
+echo "Restart terminal to apply changes"
